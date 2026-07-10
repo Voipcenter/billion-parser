24200ab0bdbbd0f9058b213c7c9d22d3809bbef7
Voipcenter <hvalipour67@gmail.com>
2026-07-10 09:56:15 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+echo "Installing Billion Parser..."
+
+cat billion-parser.conf >> /etc/asterisk/extensions_custom.conf
+
+asterisk -rx "dialplan reload"
+
+echo "Installation completed successfully."
+echo "Dial Extension: 50"
